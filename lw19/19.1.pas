68aa480e98d0b9b7e8dbe7f2dfdb2db68dbf9f39
Максим Глушков <61595112+maxglushkov@users.noreply.github.com>
2020-04-14 08:21:09 +0300

    LW19: task 1

---
+++ lw19/19.1.pas
@@ -0,0 +1,28 @@
+PROGRAM Prime(INPUT, OUTPUT);
+CONST
+  First = 2;
+  Last = 100;
+VAR
+  Item, Multiple: INTEGER;
+  Sieve: SET OF First..Last;
+BEGIN {Prime}
+  Sieve := [First..Last];
+  Item := First;
+  WHILE Item <= Last
+  DO
+    BEGIN
+      IF Item IN Sieve
+      THEN
+        BEGIN
+          WRITELN(Item);
+          Multiple := Item;
+          WHILE Multiple <= Last
+          DO
+            BEGIN
+              Sieve -= [Multiple];
+              Multiple += Item
+            END
+        END;
+      Item += 1
+    END
+END. {Prime}
